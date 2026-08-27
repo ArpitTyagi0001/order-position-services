62e6a7135c0881b9925ed72c41546b10cfb24db0
Arpit Tyagi <arpittyagi389@gmail.com>
2026-08-27 09:34:46 +0530

    add services

---
+++ position-service/src/test/java/com/position_service/PositionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.position_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
